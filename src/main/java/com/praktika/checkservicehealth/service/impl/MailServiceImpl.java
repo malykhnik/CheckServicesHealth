0860198deb2dc06c4bf2ebbd715ada29e64ebee3
Арсений <s3v3nny@ya.ru>
2024-07-02 13:34:18 +0400

    1

--- src/main/java/com/praktika/checkservicehealth/service/impl/MailServiceImpl.java
+++ src/main/java/com/praktika/checkservicehealth/service/impl/MailServiceImpl.java
@@ -4,6 +4,8 @@ import com.praktika.checkservicehealth.entity.Email;
 import com.praktika.checkservicehealth.repository.EmailRepo;
 import com.praktika.checkservicehealth.service.MailService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -16,23 +18,22 @@ public class MailServiceImpl implements MailService {
 
     private final EmailRepo emailRepo;
     private final JavaMailSender mailSender;
+    private final Logger logger = LoggerFactory.getLogger(MailService.class);
 
     @Override
     public void sendMail(String text) {
         List<Email> emails = emailRepo.findAll();
 
-        Runnable runnable = new Runnable() {
-            public void run() {
-                for(Email e : emails) {
-                    SimpleMailMessage message = new SimpleMailMessage();
-                    message.setTo(e.getReceiver());
-                    message.setSubject("Отключение сервиса");
-                    message.setText(text);
-                    mailSender.send(message);
-                }
+        Thread thread = new Thread(() -> {
+            for(Email e : emails) {
+                SimpleMailMessage message = new SimpleMailMessage();
+                message.setTo(e.getReceiver());
+                message.setSubject("Отключение сервиса");
+                message.setText(text);
+                mailSender.send(message);
+                logger.info("message sended to: {}", e.getReceiver());
             }
-        };
-        Thread thread = new Thread(runnable);
+        });
         thread.start();
     }
 }
